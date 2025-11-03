7f979db586254ddd429de3a1b2597b6be2b0e6bf
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2025-11-03 10:20:44 +0000

    up

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function checkScroll() {
     if (document.body.scrollHeight > window.innerHeight) {
-        document.body.classList.add('has-scroll');
+        document.body.classList.remove('has-not-scroll');
     } else {
-        document.body.classList.remove('has-scroll');
+        document.body.classList.add('has-not-scroll');
         document.querySelector('footer').style.display = 'block';
   }
 }
